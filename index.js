c9bf98c5718af798343e896656e97f329eb2c959
MoonLGH <MoonLMC1212@gmail.com>
2022-09-03 16:17:33 +0000

    a

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ puppeteer.use(StealthPlugin())
 
 let cookies = [{'url': 'https://zefoy.com',"name":"PHPSESSID","value":"sg34jhi7scm33pl9133jnsfrl4"}]
 async function scrape(baseUrl){
-  const browser = await puppeteer.launch();
+  const browser = await puppeteer.launch({headless:false,args: ['--no-sandbox']});
   const page = await browser.newPage();
   await page.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.4951.67 Safari/537.36 OPR/87.0.4390.58')
   await page.setCookie(...cookies)
