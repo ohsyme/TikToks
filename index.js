7806764021efcf7a2d11988c839a0cca963793e2
MoonLGH <MoonLMC1212@gmail.com>
2022-09-03 16:31:03 +0000

    a

--- index.js
+++ index.js
@@ -4,8 +4,9 @@ const StealthPlugin = require('puppeteer-extra-plugin-stealth');
 const Qs = require("qs");
 puppeteer.use(StealthPlugin())
 
+let LINKTIKTOK = ""
 let cookies = [{'url': 'https://zefoy.com',"name":"PHPSESSID","value":"sg34jhi7scm33pl9133jnsfrl4"}]
-async function scrape(baseUrl){
+async function scrape(baseUrl,Link){
   const browser = await puppeteer.launch({headless:false,args: ['--no-sandbox']});
   const page = await browser.newPage();
   await page.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.4951.67 Safari/537.36 OPR/87.0.4390.58')
@@ -15,8 +16,12 @@ async function scrape(baseUrl){
   await page.waitForXPath('/html/body/div[4]/div[1]/div[3]/div/div[4]/div/button');
   let el = await page.$x('/html/body/div[4]/div[1]/div[3]/div/div[4]/div/button')
   el[0].click()
-  await browser.close();
-  return data
+
+  await page.waitForXPath('/html/body/div[4]/div[5]/div/form/div/input');
+  // await page.focus('/html/body/div[4]/div[5]/div/form/div/input')
+  await page.focus("html > body > div:nth-of-type(4) > div:nth-of-type(5) > div > form > div > input")
+  await page.keyboard.type(Link)
+
 }
 
-scrape('https://zefoy.com')
+scrape('https://zefoy.com',LINKTIKTOK)
