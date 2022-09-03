f2455e85319ea1a600ef9b870b4bb5850d54b251
ohsyme <gamingrkp@gmail.com>
2022-09-03 20:25:30 +0700

    omaga

--- index.js
+++ index.js
@@ -13,16 +13,16 @@ async function test(){
                         "content-type": "application/x-www-form-urlencoded; charset=UTF-8",
                         "user-agent": UA.UA.random()
                     }
-    let appName       = ["tiktok_web", "musically_go"].random
+    let appName       = ["tiktok_web", "musically_go"].random()
     let Device_ID     = randomIntFromInterval(1000000000000000000, 9999999999999999999)
     let apiDomain     = json.Domain.random()
     let channelLol    = json.Channel.random()
-    let itemID = UrlParse('https://www.tiktok.com/@mau_apaan/video/7139062231605595419?is_from_webapp=1&sender_device=pc&web_id=7129868894336697857')
+    let itemID = await UrlParse('https://www.tiktok.com/@mau_apaan/video/7139062231605595419?is_from_webapp=1&sender_device=pc&web_id=7129868894336697857')
     let URI           = `https://${apiDomain}/aweme/v1/aweme/stats/?channel=${channelLol}&device_type=${DeviceType}&device_id=${Device_ID}&os_version=${osVersion}&version_code=220400&app_name=${appName}&device_platform=${platform}&aid=1988&`
     let data          = `item_id=${itemID}&play_delta=1`
 
         const proxyAgent = new HttpsProxyAgent(get_proxy());
-        const res = await fetch(URI+data, {method: 'POST', headers:headers})
+        const res = await fetch(URI+data, {method: 'POST', headers:headers, agent: proxyAgent})
         console.log((await res.text()))
         return true
 }
@@ -43,10 +43,10 @@ async function UrlParse(url) {
     }
 }
 
-async function get_proxy(){
+ function get_proxy(){
     let readfiletxt = fs.readFileSync('./Proxies.txt', "utf-8")
     let split = readfiletxt.split('\n')
-    return split.random
+    return split.random()
 
 }
 function randomIntFromInterval(min, max) { // min and max included 
